4edf6205b510e2b968fb7246d39ea1cdd698e471
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2020-10-10 13:50:01 -0700

    all done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postReducer from './postsReducer';
+import usersReducer from './usersReducer'
 
 export default combineReducers({
-  replaceMe: () => 'hi'
+  posts: postReducer,
+  users: usersReducer
 });
